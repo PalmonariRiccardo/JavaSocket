12828c2362b1b77fe471938756dd04e400c3bc99
Riccardo Palmonari <80036780+PalmonariRiccardo@users.noreply.github.com>
2022-02-22 15:37:30 +0100

    Add files via upload
    init repo

---
+++ ClientSocket.java
@@ -0,0 +1,34 @@
+package clientsocket;
+
+import java.net.*;
+import java.io.*;
+import java.util.Scanner;
+
+/**
+ *
+ * @author palmonari.riccardo
+ */
+public class ClientSocket {
+
+    public static void main(String[] args) {
+
+        String resp = null;
+        do {
+            try {
+                Scanner sc = new Scanner(System.in);
+                Socket clientSocket = new Socket("192.168.56.1", 5000);
+                PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                resp = sc.nextLine();
+                out.println(resp);
+                in.close();
+                out.close();
+                clientSocket.close();
+
+            } catch (IOException ex) {
+
+            }
+        } while (!resp.equals("fine"));
+    }
+
+}
---
+++ Server.java
@@ -0,0 +1,41 @@
+package server;
+
+import java.net.*;
+import java.io.*;
+
+/**
+ *
+ * @author palmonari.riccardo
+ */
+public class Server {
+
+    private static ServerSocket serverSocket;
+    private static Socket clientSocket;
+    private static PrintWriter out;
+    private static BufferedReader in;
+    private static int port = 5000;
+
+    public static void main(String[] args) {
+
+        try {
+            ServerSocket serverSocket = new ServerSocket(port);
+            do {
+                clientSocket = serverSocket.accept();
+                out = new PrintWriter(clientSocket.getOutputStream(), true);
+                in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                String message = in.readLine();
+                if ("fine".equals(message)) {
+                    break;
+                } else {
+                    System.out.println(message.toUpperCase());
+                }
+            } while (true);
+            in.close();
+            out.close();
+            clientSocket.close();
+            serverSocket.close();
+        } catch (IOException ex) {
+        }
+    }
+
+}
